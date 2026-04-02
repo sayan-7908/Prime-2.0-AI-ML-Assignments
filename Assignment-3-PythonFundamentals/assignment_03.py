c7d736f54e8859c704bb024d3d077c09187a9f5a
Sayan Maity <sayanmaity0014@gmail.com>
2026-04-02 19:28:52 +0530

    assignment 3

---
+++ Assignment-3-PythonFundamentals/assignment_03.py
@@ -0,0 +1,38 @@
+# #Q1.
+
+# # Ask user for a string
+# string = input("Enter a string: ")
+
+# # Reverse the string using loop
+# reversed_string = ""
+
+# for i in string:
+#     reversed_string = i + reversed_string
+
+# # Check palindrome
+# if string == reversed_string:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# #Q2.
+
+# # Input list of integers
+# numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+
+# # Calculate average
+# average = sum(numbers) / len(numbers)
+
+# # Output result
+# print("Average:", average)
+
+# #Q3.
+
+# list1 = [1, 2, 7]
+# list2 = [2, 4, 5]
+
+
+# result = list1 + list2
+# result.sort()
+
+# print(result)
